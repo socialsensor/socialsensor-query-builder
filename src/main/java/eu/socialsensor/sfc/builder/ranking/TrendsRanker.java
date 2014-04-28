77df4e67da3251d80364d2fdba01c74cf540288c
matzika <katerina.iliakopoulou@gmail.com>
2014-04-28 17:06:08 +0300

    added TrendsRanker module

---
+++ src/main/java/eu/socialsensor/sfc/builder/ranking/TrendsRanker.java
@@ -0,0 +1,13 @@
+package eu.socialsensor.sfc.builder.ranking;
+
+public class TrendsRanker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
