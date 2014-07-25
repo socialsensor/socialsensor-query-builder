c51f4e8c645535c11bdeb677ac8ab1ded09bb031
manosetro <manosetro@iti.gr>
2014-07-25 13:23:31 +0300

    Update trends ranker

--- src/main/java/eu/socialsensor/sfc/builder/ranking/TrendsRanker.java
+++ src/main/java/eu/socialsensor/sfc/builder/ranking/TrendsRanker.java
@@ -2,7 +2,6 @@ package eu.socialsensor.sfc.builder.ranking;
 
 import java.util.ArrayList;
 import java.util.Collections;
-
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -34,7 +33,7 @@ public class TrendsRanker {
 	
 	private SolrItemHandler solrItemHandler;
 	
-	public TrendsRanker(String solrCollection){
+	public TrendsRanker(String solrCollection) {
 		try {
 			solrItemHandler = SolrItemHandler.getInstance(solrCollection);
 			logger.info("SolrItemHandler initialized.. ");
@@ -50,19 +49,60 @@ public class TrendsRanker {
 	 * @param dysco
 	 * @return a double number
 	 */
-	public Double getScore(Dysco dysco){
+	public Double getScore(Dysco dysco) {
 		Double score = 0.0;
 		
 		List<Query> solrQueries = dysco.getSolrQueries();
 		
 		List<Float> queriesScores = new ArrayList<Float>();
 		
-		for(Query sQuery : solrQueries){
+		for(Query sQuery : solrQueries) {
 			float queryLength = sQuery.getName().length();
 			
 			String query = "(title : ("+sQuery.getName()+")) OR (description : ("+sQuery.getName()+"))";
 		
 			Map<Item,Float> itemsByRelevance = solrItemHandler.findItemsWithScore(query);
+		
+			float avgScore = Calculator.computeAverageFloat(itemsByRelevance.values()) * sQuery.getScore().floatValue();
+		
+			avgScore *= (queryLength/100);
+		
+			queriesScores.add(avgScore);
+		}
+		
+		long dateTimeOfDysco = dysco.getCreationDate().getTime();
+		long currentDateTime = System.currentTimeMillis();
+		
+		double timeDiff = (double) Math.abs(dateTimeOfDysco - currentDateTime)/DAY_IN_MILLISECONDS;
+		
+		double timeEval = Math.sqrt(20/(20 + (Math.exp(timeDiff))));
+	
+		score = Calculator.computeAverageFloat(queriesScores) * timeEval;
+	
+		return score;
+	}
+	
+	/**
+	 * Returns the score that was calculated for a DySco from 
+	 * comparing its content to an updated RSS collection
+	 * @param dysco
+	 * @return a double number
+	 */
+	public Double getScore(Dysco dysco, String listId) {
+		Double score = 0.0;
+		
+		List<Query> solrQueries = dysco.getSolrQueries();
+		List<Float> queriesScores = new ArrayList<Float>();
+		
+		for(Query sQuery : solrQueries) {
+			float queryLength = sQuery.getName().length();
+			
+			String query = "(title : ("+sQuery.getName()+")) OR (description : ("+sQuery.getName()+"))";
+		
+			// Solr collection of NewsFeeds needs to be fixed!
+			//query += " AND (list : " + listId + ")";
+			
+			Map<Item, Float> itemsByRelevance = solrItemHandler.findItemsWithScore(query);
 		
 			float avgScore = Calculator.computeAverageFloat(itemsByRelevance.values()) * sQuery.getScore().floatValue();
 		
@@ -90,27 +130,27 @@ public class TrendsRanker {
 	 * @param dyscos
 	 * @return
 	 */
-	public List<Dysco> rankDyscos(List<Dysco> dyscos){
+	public List<Dysco> rankDyscos(List<Dysco> dyscos) {
 		List<Dysco> rankedDyscos = new LinkedList<Dysco>();
 		
 		Map<Double,List<Dysco>> dyscosByValues = new TreeMap<Double,List<Dysco>>(Collections.reverseOrder());
 		
-		for(Dysco dysco : dyscos){
+		for(Dysco dysco : dyscos) {
 			Double score = dysco.getRankerScore();
-			if(dyscosByValues.get(score) == null){
+			if(dyscosByValues.get(score) == null) {
 				List<Dysco> alreadyIn = new ArrayList<Dysco>();
 				alreadyIn.add(dysco);
 				dyscosByValues.put(score, alreadyIn);
 			}
-			else{
+			else {
 				List<Dysco> alreadyIn = dyscosByValues.get(score);
 				alreadyIn.add(dysco);
 				dyscosByValues.put(score, alreadyIn);
 			}
 		}
 		
-		for(Map.Entry<Double, List<Dysco>> entry : dyscosByValues.entrySet()){
-			for(Dysco dysco : entry.getValue()){
+		for(Map.Entry<Double, List<Dysco>> entry : dyscosByValues.entrySet()) {
+			for(Dysco dysco : entry.getValue()) {
 				rankedDyscos.add(dysco);
 			}
 		}
@@ -126,12 +166,12 @@ public class TrendsRanker {
 	 * @param dyscos
 	 * @return the updated list of ranked DyScos.
 	 */
-	public List<Dysco> evaluateDyscosByContent(List<Dysco> dyscos){
+	public List<Dysco> evaluateDyscosByContent(List<Dysco> dyscos) {
 	
 		Map<String,String> dyscosTitles = new HashMap<String,String>();
 		Map<String,Integer> dyscosCooccurrences = new HashMap<String,Integer>();
 		
-		for(Dysco dysco : dyscos){
+		for(Dysco dysco : dyscos) {
 			List<Entity> entities = dysco.getEntities();
 			Set<String> keywords = dysco.getKeywords().keySet();
 			
@@ -139,16 +179,18 @@ public class TrendsRanker {
 			int keywordsFound = 0;
 			boolean isDuplicate = false;
 			for(Map.Entry<String, String> entry : dyscosTitles.entrySet()){
+				
 				for(Entity ent : entities){
 					if(entry.getValue().contains(ent.getName().toLowerCase()))
 						entitiesFound++;
 				}
-				for(String key : keywords){
+				
+				for(String key : keywords) {
 					if(entry.getValue().contains(key.toLowerCase()))
 						keywordsFound++;
 				}
 			
-				if((entitiesFound >=1 && keywordsFound >= 2) || dysco.getTitle().toLowerCase().equals(entry.getValue())){
+				if((entitiesFound >=1 && keywordsFound >= 2) || dysco.getTitle().toLowerCase().equals(entry.getValue())) {
 					isDuplicate = true;
 					Integer newScore = dyscosCooccurrences.get(entry.getKey()) + 1;
 					dyscosCooccurrences.put(entry.getKey(),newScore);
@@ -156,17 +198,17 @@ public class TrendsRanker {
 				}
 			}
 			
-			if(!isDuplicate){
+			if(!isDuplicate) {
 				dyscosTitles.put(dysco.getId(),dysco.getTitle().toLowerCase());
 				dyscosCooccurrences.put(dysco.getId(),1);
 				continue;
 			}
 		}
-		for(Dysco dysco : dyscos){
-			if(!dyscosCooccurrences.containsKey(dysco.getId())){
+		for(Dysco dysco : dyscos) {
+			if(!dyscosCooccurrences.containsKey(dysco.getId())) {
 				dysco.setRankerScore(-1.0);
 			}
-			else{
+			else {
 				Double rankerScore = getScore(dysco) * dyscosCooccurrences.get(dysco.getId());
 				dysco.setRankerScore(rankerScore);
 			}
@@ -177,7 +219,70 @@ public class TrendsRanker {
 	}
 	
 	
+	/**
+	 * Evaluates a set of newly created DyScos. In case they are duplicates it eliminates
+	 * them using a heuristic rule. Number of duplicates are multiplied over the score 
+	 * computed by comparing dyscos solr queries to the RSS collection. The returned DyScos
+	 * are ranked by their updated ranker scores.
+	 * @param dyscos
+	 * @return the updated list of ranked DyScos.
+	 */
+	public List<Dysco> evaluateDyscosByContent(List<Dysco> dyscos, String listId) {
 	
+		Map<String,String> dyscosTitles = new HashMap<String,String>();
+		Map<String,Integer> dyscosCooccurrences = new HashMap<String,Integer>();
+		
+		for(Dysco dysco : dyscos) {
+			List<Entity> entities = dysco.getEntities();
+			Set<String> keywords = dysco.getKeywords().keySet();
+			
+			int entitiesFound = 0;
+			int keywordsFound = 0;
+			boolean isDuplicate = false;
+			
+			for(Map.Entry<String, String> entry : dyscosTitles.entrySet()) {
+				
+				String dyscoId = entry.getKey();
+				String dyscoTitle = entry.getValue();
+				
+				for(Entity ent : entities) {
+					if(dyscoTitle.contains(ent.getName().toLowerCase()))
+						entitiesFound++;
+				}
+				
+				for(String keyword : keywords) {
+					if(dyscoTitle.contains(keyword.toLowerCase()))
+						keywordsFound++;
+				}
+			
+				if((entitiesFound >=1 && keywordsFound >= 2) || dysco.getTitle().toLowerCase().equals(dyscoTitle)) {
+					isDuplicate = true;
+					Integer newScore = dyscosCooccurrences.get(dyscoId) + 1;
+					dyscosCooccurrences.put(dyscoId, newScore);
+					break;
+				}
+			}
+			
+			if(!isDuplicate) {
+				dyscosTitles.put(dysco.getId(), dysco.getTitle().toLowerCase());
+				dyscosCooccurrences.put(dysco.getId(), 1);
+				continue;
+			}
+		}
+		
+		for(Dysco dysco : dyscos) {
+			if(!dyscosCooccurrences.containsKey(dysco.getId())) {
+				dysco.setRankerScore(-1.0);
+			}
+			else {
+				Double rankerScore = getScore(dysco, listId) * dyscosCooccurrences.get(dysco.getId());
+				dysco.setRankerScore(rankerScore);
+			}
+		}
+		
+		
+		return rankDyscos(dyscos);
+	}
 
 	/**
 	 * @param args
